f9f2b33e9d037fc0ee93340aa5c7b2fbda8c3d40
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-30 00:45:28 +0200

    Add a circle to the skeltons.

--- src/components/Posts/PostLoader.tsx
+++ src/components/Posts/PostLoader.tsx
@@ -13,11 +13,13 @@ const PostLoader = (props: Props) => {
   return (
     <Stack spacing={6}>
       <Box padding="10px 10px" boxShadow="lg" bg="white" borderRadius={4}>
+        <SkeletonCircle size="10" />
         <SkeletonText mt="4" noOfLines={1} width="40%" spacing="4" />
         <SkeletonText mt="4" noOfLines={4} spacing="4" />
         <Skeleton mt="4" height="200px" />
       </Box>
       <Box padding="10px 10px" boxShadow="lg" bg="white" borderRadius={4}>
+        <SkeletonCircle size="10" />
         <SkeletonText mt="4" noOfLines={1} width="40%" spacing="4" />
         <SkeletonText mt="4" noOfLines={4} spacing="4" />
         <Skeleton mt="4" height="200px" />
