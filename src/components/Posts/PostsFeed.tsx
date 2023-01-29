86573c8dcaf387582eb668097215bef3f20e88e1
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-29 22:49:21 +0200

    Add usePosts and fetch the posts from firebase.

--- src/components/Posts/PostsFeed.tsx
+++ src/components/Posts/PostsFeed.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import { User } from "firebase/auth";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { Community } from "@/atoms/communities.Atom";
+import { firestore } from "@/firebase/clientApp";
+import usePosts from "@/hooks/usePosts";
+import { IPost } from "../../atoms/posts.Atom";
 
-type Props = {};
+type Props = {
+  communityData: Community;
+  user?: User;
+};
 
-const PostsFeed = (props: Props) => {
+const PostsFeed = ({ communityData }: Props) => {
+  const { postStateValue, setPostStateValue } = usePosts();
+
+  const getPosts = async () => {
+    try {
+      const postsQuery = query(
+        collection(firestore, "posts"),
+        where("community", "==", communityData.id),
+        orderBy("createdAt", "desc")
+      );
+      const postDocs = await getDocs(postsQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+      setPostStateValue((prev) => ({ ...prev, posts: posts as IPost[] }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
   return <div>PostsFeed</div>;
 };
 
