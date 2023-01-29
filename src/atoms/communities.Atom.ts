c67ea6b11c516f19cd1c571b586f3bb003e9f32a
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-29 06:30:01 +0200

    Add community atom

---
+++ src/atoms/communities.Atom.ts
@@ -0,0 +1,32 @@
+import { Timestamp } from "firebase/firestore";
+import { atom } from "recoil";
+
+export interface Community {
+  id: string;
+  creatorId: string;
+  numberOfMembers: number;
+  privacyType: "public" | "restrictied" | "private";
+  createdAt?: Timestamp;
+  // make a default url instead of optional
+  imageURL?: string;
+}
+
+export interface CommunitySnippet {
+  communityId: string;
+  isModerator?: boolean;
+  imageURL?: string;
+}
+
+interface CommunityState {
+  mySnippets: CommunitySnippet[];
+  // visitedCommunities:
+}
+
+export const defaultCommunityState: CommunityState = {
+  mySnippets: [],
+};
+
+export const communityState = atom<CommunityState>({
+  key: "communitiesState",
+  default: defaultCommunityState,
+});
