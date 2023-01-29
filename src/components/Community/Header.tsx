38cacd82009aab12e5d49512a06dd1fadbafe772
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-29 08:37:25 +0200

    Add basic communit header

---
+++ src/components/Community/Header.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Community } from "@/atoms/communities.Atom";
+import { Box, Button, Flex, Icon, Image, Text } from "@chakra-ui/react";
+import { FaReddit } from "react-icons/fa";
+import useCommunityData from "@/hooks/useCommunityData";
+
+type Props = {
+  communityData: Community;
+};
+
+const CommunityHeader = ({ communityData }: Props) => {
+  const { communityStateValue, onJoinOrLeaveCommunity, loading } =
+    useCommunityData();
+
+  // will come from the community snippet (From a state)
+  const isJoined = !!communityStateValue.mySnippets.find(
+    ({ communityId }) => communityId === communityData.id
+  );
+
+  return (
+    <Flex
+      direction="column"
+      w="100%"
+      h="146px"
+      bgGradient="linear-gradient(blue.400 50%, white 50% 100%)"
+      justify="end"
+      align="center"
+      p="16px"
+    >
+      <Flex w="95%" h="5rem" maxW="860px">
+        {communityData.imageURL ? (
+          <Image
+            borderRadius="full"
+            boxSize="66px"
+            src={communityData.imageURL}
+            alt={communityData.id}
+            color="blue.500"
+            border="4px solid white"
+            bg="white"
+          />
+        ) : (
+          <Icon
+            as={FaReddit}
+            fontSize={64}
+            color="blue.500"
+            border="3px solid white"
+            borderRadius="50%"
+            bg="white"
+          />
+        )}
+        <Flex p="30px 16px 0 16px">
+          <Flex direction="column" justify="center" mr="6">
+            <Text fontWeight="800" fontSize="16pt">
+              {communityData.id}
+            </Text>
+            <Text fontWeight="600" fontSize="10pt" color="gray.400">
+              r/{communityData.id}
+            </Text>
+          </Flex>
+          <Button
+            variant={isJoined ? "outline" : "solid"}
+            height="30px"
+            pr={6}
+            pl={6}
+            onClick={() => {
+              onJoinOrLeaveCommunity(communityData, isJoined);
+            }}
+            isLoading={loading}
+          >
+            {isJoined ? "Joined" : "Join"}
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default CommunityHeader;
