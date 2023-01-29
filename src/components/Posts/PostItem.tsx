f2950d90d54d29acef73513d7d0a622ee0c73736
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-30 00:19:34 +0200

    Add title, body, image, reactions buttons UI.

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -8,7 +8,7 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
-// import moment from "moment";
+import moment from "moment";
 import { NextRouter } from "next/router";
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsChat, BsDot } from "react-icons/bs";
@@ -29,7 +29,7 @@ type Props = {
   userIsAuther: boolean;
   userVoteValue?: 1 | -1 | 0;
   onVote: () => {};
-  onPostDelete: () => {};
+  onPostDelete: (postId: IPost["id"]) => {};
   onSelectedPost: () => {};
 };
 
@@ -81,7 +81,72 @@ const PostItem = ({
           cursor="pointer"
         />
       </Flex>
-      {post.title}
+      <Flex direction="column" width="100%">
+        <Stack spacing={1} p="10px 10px">
+          <Stack direction="row" spacing={0.6} align="center" fontSize="9pt">
+            {/* Check for Home Page */}
+            <Text>
+              Posted by u/{post.authorName}{" "}
+              {moment(new Date(post.createdAt.seconds * 1000)).fromNow(false)}
+            </Text>
+          </Stack>
+          <Text fontSize="12pt" fontWeight={600}>
+            {post.title}
+          </Text>
+          <Text fontSize="10pt">{post.body}</Text>
+          {post.imageURL && (
+            <Flex justify="center" align="center" p={2}>
+              {/* TO_DO : make this alt dynamic */}
+              <Image src={post.imageURL} alt={"post image"} />
+            </Flex>
+          )}
+        </Stack>
+        <Flex ml="1" mb="0.5" color="gray.500">
+          <Flex
+            align="center"
+            p="8px 10px"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+            cursor="pointer"
+          >
+            <Icon as={BsChat} mr="2" />
+            <Text fontSize="9pt">{post.numberOfComments}</Text>
+          </Flex>
+          <Flex
+            align="center"
+            p="8px 10px"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+            cursor="pointer"
+          >
+            <Icon as={IoArrowRedoOutline} mr="2" />
+            <Text fontSize="9pt">Share</Text>
+          </Flex>
+          <Flex
+            align="center"
+            p="8px 10px"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+            cursor="pointer"
+          >
+            <Icon as={IoBookmarkOutline} mr="2" />
+            <Text fontSize="9pt">Save</Text>
+          </Flex>
+          {userIsAuther && (
+            <Flex
+              align="center"
+              p="8px 10px"
+              borderRadius={4}
+              _hover={{ bg: "gray.200" }}
+              cursor="pointer"
+              onClick={() => onPostDelete(post.id)}
+            >
+              <Icon as={AiOutlineDelete} mr="2" />
+              <Text fontSize="9pt">Delete</Text>
+            </Flex>
+          )}
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
