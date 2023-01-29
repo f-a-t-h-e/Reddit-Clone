03b2398975e16e80109b9a97167823f7901a9718
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-29 16:37:46 +0200

    Add postsState.

---
+++ src/atoms/posts.Atom.ts
@@ -0,0 +1,32 @@
+import { Timestamp } from "firebase/firestore";
+import { atom } from "recoil";
+
+export interface IPost {
+  id: string;
+  communityId: string;
+  authorId: string;
+  authorName: string;
+  title: string;
+  body: string;
+  numberOfComments: string;
+  voteStatus: string;
+  imageURL?: string;
+  communityImageURL?: string;
+  createdAt: Timestamp;
+}
+
+interface IPostState {
+  selectedPost: IPost | null;
+  posts: IPost[];
+  // postVotes:number
+}
+
+const defaultPostState: IPostState = {
+  selectedPost: null,
+  posts: [],
+};
+
+export const postState = atom({
+  key: "POSTS_STATE",
+  default: defaultPostState,
+});
