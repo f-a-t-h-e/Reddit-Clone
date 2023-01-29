33e64816e8255d630cdf43e16cbba83688ac3f76
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-29 21:09:01 +0200

    Pass user to NewPostForm component.

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import PageConentLayout from "@/components/Layout/PageConent";
 import { Box, Text } from "@chakra-ui/react";
 import NewPostForm from "@/components/Posts/PostForm/NewPostForm";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 type Props = {};
 
 const SubmitPostPage = (props: Props) => {
+  const [user] = useAuthState(auth);
   return (
     <PageConentLayout>
       <>
         <Box p="14px 0px" borderBottom="1px solid " borderColor="white">
           <Text>Create a Post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />}
       </>
       <></>
     </PageConentLayout>
