e89dbbc7b011ac3680b0952d8a2436cd9e1bbf69
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-30 09:55:45 +0200

    Handle Post delete.

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,11 +1,33 @@
 import { useRecoilState } from "recoil";
-import { postState } from "@/atoms/posts.Atom";
+import { IPost, postState } from "@/atoms/posts.Atom";
+import { deleteObject, ref } from "firebase/storage";
+import { firestore, storage } from "../firebase/clientApp";
+import { deleteDoc, doc } from "firebase/firestore";
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
 
   const onVote = async () => {};
-  const onPostDelete = async () => {};
+  const onPostDelete = async (post: IPost): Promise<boolean> => {
+    try {
+      // Check if there is an image to remove it
+      if (post.imageURL) {
+        const imageRef = ref(storage, `posts/${post.id}/imageURL`);
+        await deleteObject(imageRef);
+      }
+
+      const postDocRef = doc(firestore, "posts", post.id);
+      await deleteDoc(postDocRef);
+
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter(({ id }) => id !== post.id),
+      }));
+    } catch (error) {
+      return false;
+    }
+    return true;
+  };
   const onSelectedPost = async () => {};
   const getCommunityPostVotes = async () => {};
 
