e7a1c21b1d46146d53a879415f622ae8229514fc
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-30 21:31:21 +0200

    Redirect the user prepoerly.

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,18 +1,30 @@
-import React from "react";
-import PageConentLayout from "@/components/Layout/PageConent";
-import { Box, Text } from "@chakra-ui/react";
-import NewPostForm from "@/components/Posts/PostForm/NewPostForm";
+import React, { useEffect } from "react";
+
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/clientApp";
+import { useRouter } from "next/router";
 import { useRecoilValue } from "recoil";
+
+import { Box, Text } from "@chakra-ui/react";
+
+import NewPostForm from "@/components/Posts/PostForm/NewPostForm";
+import PageConentLayout from "@/components/Layout/PageConent";
 import { communityState } from "@/atoms/communities.Atom";
 
 type Props = {};
 
-const SubmitPostPage = (props: Props) => {
+const SubmitPostPage = ({}: Props) => {
   const [user] = useAuthState(auth);
   const communityStateValue = useRecoilValue(communityState);
+  const router = useRouter();
 
+  useEffect(() => {
+    if (!communityStateValue.currentCommunity) {
+      router.push("/r/");
+    } else if (!user && communityStateValue.currentCommunity.id) {
+      router.push(`/r/${communityStateValue.currentCommunity.id}`);
+    }
+  }, [user, communityStateValue.currentCommunity, router]);
   return (
     <PageConentLayout>
       <>
