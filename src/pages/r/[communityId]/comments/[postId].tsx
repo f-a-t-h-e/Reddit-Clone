8c00dc9e09eaff6dc635357bb582ed1066184955
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-30 22:13:23 +0200

    Add PostPage route.

---
+++ src/pages/r/[communityId]/comments/[postId].tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PageConentLayout from "@/components/Layout/PageConent";
+
+type Props = {};
+
+const PostPage = ({}: Props) => {
+  return (
+    <PageConentLayout>
+      <>
+        {/* <SelectedPost /> */}
+        {/* <Comments /> */}
+      </>
+      <>{/* <About /> */}</>
+    </PageConentLayout>
+  );
+};
+
+export default PostPage;
