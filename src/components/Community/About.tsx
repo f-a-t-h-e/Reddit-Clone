50f13c7991cb1d62df53d9ce24c9bc442002761b
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-30 10:48:07 +0200

    Create CommunityAbout component.

---
+++ src/components/Community/About.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Community } from "@/atoms/communities.Atom";
+
+type Props = {
+  communityData: Community;
+};
+
+const About = ({ communityData }: Props) => {
+  return <div>About</div>;
+};
+
+export default About;
