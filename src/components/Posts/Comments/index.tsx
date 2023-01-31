743cc11ea86ae68467d1e00f9add8ef57db0d632
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-31 17:55:41 +0200

    Add fetching functionality.

--- src/components/Posts/Comments/index.tsx
+++ src/components/Posts/Comments/index.tsx
@@ -1,10 +1,20 @@
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Flex,
+  SkeletonCircle,
+  SkeletonText,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import type { User } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { IPost } from "@/atoms/posts.Atom";
 import CommentInput from "./CommentInput";
 import type { IComment } from "./types";
 import CommentItem from "./CommentItem";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { firestore } from "@/firebase/clientApp";
 
 type Props = {
   user?: User | null;
@@ -26,10 +36,29 @@ const Comments = ({ communityId, selectedPost, user }: Props) => {
     return false;
   };
 
-  const getPostComments = async () => {};
+  const getPostComments = async () => {
+    setFetchLoading(true);
+    try {
+      const commentsQuery = query(
+        collection(firestore, "comments"),
+        where("postId", "==", selectedPost.id),
+        orderBy("createdAt", "desc")
+      );
+
+      const commentDocs = await getDocs(commentsQuery);
+      const comments = commentDocs.docs.map(
+        (doc) => ({ id: doc.id, ...doc.data() } as IComment)
+      );
+      setComments(comments);
+    } catch (error) {
+      console.log("🚀 ~ file: index.tsx:41 ~ getPostComments ~ error", error);
+    }
+    setFetchLoading(false);
+  };
 
   useEffect(() => {
     getPostComments();
+    // TO_DO : check if you need to add "selectedPost" as a dependency
   }, []);
   return (
     // TO_DO : use a <Stack />
@@ -42,24 +71,44 @@ const Comments = ({ communityId, selectedPost, user }: Props) => {
         fontSize="10pt"
         w="100%"
       >
-        <CommentInput
-          createLoading={createLoading}
-          post={selectedPost}
-          user={user}
-          setCreateLoading={setCreateLoading}
-          setComments={setComments}
-        />
+        {!fetchLoading && (
+          <CommentInput
+            createLoading={createLoading}
+            post={selectedPost}
+            user={user}
+            setCreateLoading={setCreateLoading}
+            setComments={setComments}
+          />
+        )}
       </Flex>
       <Stack spacing={6}>
-        {comments.map((comment, i) => (
-          <CommentItem
-            key={i}
-            comment={comment}
-            user={user}
-            onCommentDelete={onCommentDelete}
-            isLoading={deleteLoading}
-          />
-        ))}
+        {fetchLoading ? (
+          <>
+            {[0, 1, 2].map((item) => (
+              <Box key={item} padding="6" bg="white">
+                <SkeletonCircle size="10" />
+                <SkeletonText mt="4" noOfLines={2} spacing="4" />
+              </Box>
+            ))}
+          </>
+        ) : comments.length ? (
+          comments.map((comment, i) => (
+            <CommentItem
+              key={i}
+              comment={comment}
+              user={user}
+              onCommentDelete={onCommentDelete}
+              isLoading={deleteLoading}
+            />
+          ))
+        ) : (
+          <>
+            <Divider />
+            <Text align="center" fontWeight={700} opacity={0.3} p={20}>
+              No Comments Yet
+            </Text>
+          </>
+        )}
       </Stack>
     </Box>
   );
