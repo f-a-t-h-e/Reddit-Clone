ca9e130ad7f59453f636c65961ef592f89e4f821
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-31 09:33:33 +0200

    Create basic structure.

--- src/components/Posts/Comments/index.tsx
+++ src/components/Posts/Comments/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { Stack } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import React, { useEffect } from "react";
+import { IPost } from "@/atoms/posts.Atom";
 
-type Props = {};
+type Props = {
+  user?: User | null;
+  selectedPost: IPost;
+  communityId: string;
+};
 
-const Comments = (props: Props) => {
-  return <div>Comments</div>;
+const Comments = ({ communityId, selectedPost, user }: Props) => {
+  const onCreateComment = async (commentText: string) => {};
+
+  const onCommentDelete = async (comment: any) => {};
+
+  const getPostComments = async () => {};
+
+  useEffect(() => {
+    getPostComments();
+  }, []);
+  return <Stack spacing="1">Comments</Stack>;
 };
 
 export default Comments;
