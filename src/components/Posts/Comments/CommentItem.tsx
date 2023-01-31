7dd426d4a7faebdb2d4663aa7cd25d0eeb258295
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-31 16:47:14 +0200

    Add basic UI

--- src/components/Posts/Comments/CommentItem.tsx
+++ src/components/Posts/Comments/CommentItem.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import type { IComment } from "./types";
 
+import moment from "moment";
+import { FaReddit } from "react-icons/fa";
+import {
+  IoArrowDownCircleOutline,
+  IoArrowUpCircleOutline,
+} from "react-icons/io5";
+import { Box, Flex, Icon, Stack, Text } from "@chakra-ui/react";
+
 type Props = {
   comment: IComment;
   onDeleteComment: (comment: IComment) => Promise<void>;
@@ -14,7 +22,21 @@ const CommentItem = ({
   onDeleteComment,
   userId,
 }: Props) => {
-  return <div></div>;
+  return (
+    <Flex>
+      <Box mr={2}>
+        <Icon as={FaReddit} />
+      </Box>
+      <Stack spacing={1}>
+        <Stack direction="row" align="center" fontSize="8pt">
+          <Text>{comment.authorName}</Text>
+          <Text>
+            {moment(new Date(comment.createdAt.seconds * 1000)).fromNow()}
+          </Text>
+        </Stack>
+      </Stack>
+    </Flex>
+  );
 };
 
 export default CommentItem;
