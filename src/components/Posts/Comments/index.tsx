7a6038b2797b047f1e8167880a4ede5b173f2d31
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-01-31 10:07:04 +0200

    Fill initial structure.

--- src/components/Posts/Comments/index.tsx
+++ src/components/Posts/Comments/index.tsx
@@ -1,7 +1,9 @@
-import { Stack } from "@chakra-ui/react";
+import { Box, Flex, Stack } from "@chakra-ui/react";
 import { User } from "firebase/auth";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { IPost } from "@/atoms/posts.Atom";
+import Comment from "./Comment";
+import CommentInput from "./CommentInput";
 
 type Props = {
   user?: User | null;
@@ -10,7 +12,14 @@ type Props = {
 };
 
 const Comments = ({ communityId, selectedPost, user }: Props) => {
-  const onCreateComment = async (commentText: string) => {};
+  const [commentText, setCommentText] = useState("");
+  const [comments, setComments] = useState([]);
+  const [fetchLoading, setFetchLoading] = useState(true);
+  const [createLoading, setCreateLoading] = useState(false);
+
+  const onCreateComment = async (commentText: string) => {
+    return false;
+  };
 
   const onCommentDelete = async (comment: any) => {};
 
@@ -19,7 +28,21 @@ const Comments = ({ communityId, selectedPost, user }: Props) => {
   useEffect(() => {
     getPostComments();
   }, []);
-  return <Stack spacing="1">Comments</Stack>;
+  return (
+    // TO_DO : use a <Stack />
+    <Box bg="white" borderRadius="0px 0px 4px 4px" p="0.5rem">
+      <Flex
+        direction="column"
+        pl="2.5rem"
+        pr="1rem"
+        mb="1.5rem"
+        fontSize="10pt"
+        w="100%"
+      >
+        {/* <CommentInput /> */}
+      </Flex>
+    </Box>
+  );
 };
 
 export default Comments;
