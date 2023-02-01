92b16b531381299798bf1ff35571f272dcce4726
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-01 06:30:43 +0200

    Create Menu Item. for use in the directory menu.

---
+++ src/components/Navbar/Directory/MenuListItem.tsx
@@ -0,0 +1,47 @@
+import { Flex, Icon, Image, MenuItem } from "@chakra-ui/react";
+import React from "react";
+import { IconType } from "react-icons/lib";
+
+type Props = {
+  displayText: string;
+  link: string;
+  icon: IconType;
+  iconColor: string;
+  imageURL?: string;
+};
+
+const MenuListItem = ({
+  displayText,
+  icon,
+  iconColor,
+  link,
+  imageURL,
+}: Props) => {
+  console.log(imageURL);
+
+  return (
+    <MenuItem
+      w="100%"
+      fontSize="10pt"
+      _hover={{ bg: "gray.100" }}
+      onClick={() => {}}
+    >
+      <Flex align="center">
+        {imageURL ? (
+          <Image
+            src={imageURL}
+            borderRadius="full"
+            boxSize="18px"
+            mr={2}
+            alt={displayText}
+          />
+        ) : (
+          <Icon as={icon} mr={2} color={iconColor} fontSize={20} />
+        )}
+        {displayText}
+      </Flex>
+    </MenuItem>
+  );
+};
+
+export default MenuListItem;
