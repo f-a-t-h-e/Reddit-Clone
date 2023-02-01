24273f7b5ec7453453bfb931747be5a789846a6d
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-01 07:10:49 +0200

    Show the communities. the communities that the user is moderating.

--- src/components/Navbar/Directory/Communities.tsx
+++ src/components/Navbar/Directory/Communities.tsx
@@ -1,8 +1,8 @@
 import { Box, Flex, MenuItem, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GrAdd } from "react-icons/gr";
 import { useRecoilValue } from "recoil";
-import { communityState } from "@/atoms/communities.Atom";
+import { CommunitySnippet, communityState } from "@/atoms/communities.Atom";
 import CreateCommunityModal from "@/components/Modal/CreateCommunity";
 import MenuListItem from "./MenuListItem";
 import { FaReddit } from "react-icons/fa";
@@ -12,9 +12,37 @@ type Props = {};
 const Communities = (props: Props) => {
   const [open, setOpen] = useState(false);
   const { mySnippets } = useRecoilValue(communityState);
+
   return (
     <>
       <CreateCommunityModal open={open} onClose={() => setOpen(false)} />
+      {mySnippets.filter((snip) => snip.isMod) &&
+        ((
+          <Box mt={3} mb={4}>
+            <Text
+              pl={3}
+              mb={1}
+              fontSize="7pt"
+              fontWeight={500}
+              color="gray.500"
+            >
+              MODIRATING
+            </Text>
+            {mySnippets.map(
+              (snip) =>
+                snip.isMod && (
+                  <MenuListItem
+                    key={snip.communityId}
+                    displayText={`r/${snip.communityId}`}
+                    link={`/r/${snip.communityId}`}
+                    icon={FaReddit}
+                    iconColor="brand.100"
+                  />
+                )
+            )}
+          </Box>
+        ) ||
+          10)}
       <Box mt={3} mb={4}>
         <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
           MY COMMUNITIES
