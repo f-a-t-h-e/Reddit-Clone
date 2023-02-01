34dd4b4c7257d034023bedb53dd4d6033ba8094d
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-01 06:40:47 +0200

    Add CommunitySnippet UI.

--- src/components/Navbar/Directory/Communities.tsx
+++ src/components/Navbar/Directory/Communities.tsx
@@ -1,15 +1,25 @@
-import { Flex, MenuItem } from "@chakra-ui/react";
+import { Box, Flex, MenuItem, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { GrAdd } from "react-icons/gr";
-import CreateCommunityModal from "../../Modal/CreateCommunity";
+import { useRecoilValue } from "recoil";
+import { communityState } from "@/atoms/communities.Atom";
+import CreateCommunityModal from "@/components/Modal/CreateCommunity";
+import MenuListItem from "./MenuListItem";
+import { FaReddit } from "react-icons/fa";
 
 type Props = {};
 
 const Communities = (props: Props) => {
   const [open, setOpen] = useState(false);
+  const { mySnippets } = useRecoilValue(communityState);
   return (
     <>
       <CreateCommunityModal open={open} onClose={() => setOpen(false)} />
+      <Box mt={3} mb={4}>
+        <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
+          MY COMMUNITIES
+        </Text>
+      </Box>
       <MenuItem
         fontSize="10pt"
         _hover={{ bg: "gray.100" }}
@@ -18,6 +28,16 @@ const Communities = (props: Props) => {
       >
         Create Community
       </MenuItem>
+      {mySnippets.map((snip, i) => (
+        <MenuListItem
+          key={i}
+          displayText={`r/${snip.communityId}`}
+          icon={FaReddit}
+          iconColor={"blue.500"}
+          link={`/r/${snip.communityId}`}
+          imageURL={snip.imageURL}
+        />
+      ))}
     </>
   );
 };
