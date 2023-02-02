b643b7d34e973bc6636b0ceb72ebb7ee02fd4b98
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-02 21:48:51 +0200

    Add fetching postFeed functionality.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import { Inter } from "@next/font/google";
 import { NextPage } from "next";
 import PageConentLayout from "@/components/Layout/PageConent";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/clientApp";
+import { auth, firestore } from "@/firebase/clientApp";
 import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { communityState } from "@/atoms/communities.Atom";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { IPost } from "@/atoms/posts.Atom";
+import usePosts from "@/hooks/usePosts";
+import PostLoader from "@/components/Posts/PostLoader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +19,26 @@ const Home: NextPage = () => {
   const [user, loadingUser] = useAuthState(auth);
   const [loading, setLoading] = useState(false);
   const communityStateValue = useRecoilValue(communityState);
+  const { setPostStateValue } = usePosts();
 
   const buildUserHomeFeed = async () => {
     setLoading(true);
     try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map(
+        (doc) => ({ id: doc.id, ...doc.data() } as IPost)
+      );
+
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts,
+      }));
     } catch (error) {
       console.log("🚀 ~ file: index.tsx:23 ~ buildUserHomeFeed ~ error", error);
     }
